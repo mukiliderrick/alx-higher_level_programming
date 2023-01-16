0a02f4236111578f94960ddbbf513ff63efb0728
mukiliderrick <derrickmukili64@gmail.com>
2023-01-16 15:46:11 +0300

    fin

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""modules for the class Base"""
+import json
+import csv
+import os.path
+
+class Base:
+    """Class Base"""
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        """initialise the instances"""
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects +=1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from models.base import Base
+class Rectangle:
+    __width
+    __height
+    __x
+    __y
+    def __ini__(self, width, height, x=0,y=0,id=None):
+        
