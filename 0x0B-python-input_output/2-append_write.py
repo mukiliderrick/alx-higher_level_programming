1623474500e2485b53048f8141523c7a78219cc9
mukiliderrick <derrickmukili64@gmail.com>
2023-01-17 15:34:28 +0300

    team 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python3
+"""module that appends string at the end of a file"""
+
+
 def append_write(filename="",text=""):
+    """function that appends a character"""
     with open(filename, "a", encoding="utf-8") as file:
         num_chars = file.write(text)
     return num_chars
