f4691d57d6a75bc034d0cafa9827ce7538aff73d
mukiliderrick <derrickmukili64@gmail.com>
2023-01-17 15:22:36 +0300

    file read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""module for reading a file"""
 
 def read_file(filename=""):
+    """function that reads a file"""
     with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read())
+        read_data = file.read()
+        print(read_data, end='')
